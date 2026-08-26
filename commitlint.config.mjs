8a91bfc5a6846083a1a4732bbd6967b46b7d14af
Ayman Errarhiche <meta.ayman@gmail.com>
2026-08-26 05:56:45 +0100

    chore(commitlint): enforce conventional commit messages (config + husky + deps)

---
+++ commitlint.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [
+    (message) => message.startsWith("Merge "),
+    (message) => message.startsWith("Revert "),
+    (message) => message.startsWith("fixup!"),
+    (message) => message.startsWith("squash!"),
+  ],
+};
